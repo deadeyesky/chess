185064528b2bdf8a8f7243848d5822b29b9cd565
deadeyesky <53882236+deadeyesky@users.noreply.github.com>
2024-02-02 09:43:21 -0700

    Update ChessMove.java
    Removed the record

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,17 +8,52 @@ import java.util.Objects;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public record ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
-    @Override
-    public String toString() {
-        return String.format("%s (Promo: %s)", endPosition, promotionPiece);
+public class ChessMove {
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
+
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
+                     ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
+
+    /**
+     * @return ChessPosition of starting location
+     */
+    public ChessPosition getStartPosition() {
+        return startPosition;
+    }
+
+    /**
+     * @return ChessPosition of ending location
+     */
+    public ChessPosition getEndPosition() {
+        return endPosition;
+    }
+
+    /**
+     * Gets the type of piece to promote a pawn to if pawn promotion is part of this
+     * chess move
+     *
+     * @return Type of piece to promote a pawn to, or null if no promotion
+     */
+    public ChessPiece.PieceType getPromotionPiece() {
+        return promotionPiece;
     }
 
     @Override
     public boolean equals(Object object) {
-        if (this == object) return true;
+        if (object == this) return true;
         if (object == null || getClass() != object.getClass()) return false;
         ChessMove move = (ChessMove) object;
         return Objects.equals(startPosition, move.startPosition) && Objects.equals(endPosition, move.endPosition) && promotionPiece == move.promotionPiece;
+    }
+
+    @Override
+    public String toString () {
+        return String.format("%s (Proms: %s)", endPosition, promotionPiece);
     }
 }
